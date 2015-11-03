eed583a80d23612cd57f5d8cf602d111c2d885bb
branespace@users.noreply.github.com <branespace@users.noreply.github.com>
2015-11-02 17:38:36 -0800

    Add gulp, tasks, linting

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var gulp = require('gulp');
+var gulpmocha = require('gulp-mocha');
+var jscs = require('gulp-jscs');
+var jshint = require('gulp-jshint');
+var lintableFiles = ['!node_modules/*/**', '**/*.js'];
+
+gulp.task('jshint', function() {
+  return gulp.src(lintableFiles)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default', {verbose: true}));
+});
+
+gulp.task('jscs', function() {
+  return gulp.src(lintableFiles)
+    .pipe(jscs())
+    .pipe(jscs.reporter());
+});
+
+gulp.task('lint', ['jshint', 'jscs']);
+
+gulp.task('default', ['lint']);
